ab3dc3e321c48f0e543a9eba6da9c0f598ed3925
kumarsantosh22 <santoshcse97@gmail.com>
2021-08-18 10:37:04 +0530

    ainitial commit

---
+++ decision.js
@@ -0,0 +1,53 @@
+var labels = ['Decision Makers / 30Exported', 'Influencers', 'Strengths/Teams'];
+var data = {
+  labels: labels,
+  datasets: [
+    {
+      axis: "y",
+      // label: "",
+      data: [50, 60, 40],
+      fill: false,
+      backgroundColor: [
+        "rgba(44, 130, 201, 1)",
+
+        "rgba(0, 181, 204, 1)",
+
+        "rgba(82, 179, 217, 1)",
+      ],
+      borderWidth: 1,
+      borderRadius: 4,
+      barThickness: 8,
+    },
+
+    {
+      axis: "y",
+      // label: "",
+      data: [30, 35, 38],
+      fill: false,
+      backgroundColor: [
+        "rgba(137, 196, 244, 1)",
+
+        "rgba(65, 131, 215, 1)",
+
+        "rgba(34, 167, 240, 1)",
+      ],
+      borderWidth: 1,
+      borderRadius: 4,
+      barThickness: 8,
+    },
+  ],
+};
+
+var config = {
+  type: "bar",
+  data,
+  options: {
+    indexAxis: "y",
+    legend: {
+      display: false,
+    },
+  },
+};
+
+
+var myChart = new Chart(document.getElementById("decisionChart"), config);
---
+++ revenue.js
@@ -0,0 +1,32 @@
+var data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(70,130,180)",
+        "rgb(30,144,255)",
+        "rgb(135,206,235)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+var config = {
+  type: "doughnut",
+  data: data,
+};
+
+data = {
+  datasets: [
+    {
+      data: [10, 20, 30],
+    },
+  ],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: ["Red", "Yellow", "Blue"],
+};
+var myChart = new Chart(document.getElementById("revenueChart"), config);
